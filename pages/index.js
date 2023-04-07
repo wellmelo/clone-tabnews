6b572ab3e65417ffe2c8d7f9627ff08d793d6aa7
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-07 06:05:03 +0000

    Home Amor Com Conteudo

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ function Home() {
                 Oi meu amor!
                 Fiz essa pequena página para<br>
                 Te lembrar que eu<br>
-                <h2>Te Amo</h2><br>
-                Mesmo você querendo me matar todos os dias!<br>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+                <h2 style={color:red;}>Te Amo</h2><br>
+                Mesmo você querendo me matar todos os dias!<br>
+                <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755" width={205}></img>
             </center>
         </div>
     )
