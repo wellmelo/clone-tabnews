76fb73edec9c9dcafd11496d04c9e84f83634930
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-07 06:28:17 +0000

    Pagina Te Amo

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
 function Home() {
-    return ( 
+    return (
         <div>
-            <center>
-                Oi meu amor!
-                Fiz essa pequena página para<br>
-                Te lembrar que eu<br>
-                <h2 style={color:red;}>Te Amo</h2><br>
-                Mesmo você querendo me matar todos os dias!<br>
-                <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755" width={205}></img>
-            </center>
+            Oi meu amor! Fiz essa pequena página para dizer que eu <h2 style={color:red;}>Te Amo</h2>
         </div>
     )
 }
