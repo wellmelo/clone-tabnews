93e80a4ca2e9e1432568da933eb8749b30e0fb1f
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-07 05:36:08 +0000

    funcia Homeona p, amor

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 function Home() {
     return ( 
         <div>
-            Oi meu amor!
-            Fiz essa pequena página para
-            Te lembrar que eu<br>
-            <h2 style="color:red;">Te Amo</h2>
-            Mesmo você querendo me matar todos os dias!
-            <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755" width="205">
+            <center>
+                Oi meu amor!
+                Fiz essa pequena página para<br>
+                Te lembrar que eu<br>
+                <h2>Te Amo</h2><br>
+                Mesmo você querendo me matar todos os dias!<br>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            </center>
         </div>
     )
 }
