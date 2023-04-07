b25e487c1ea94ab6ddf56dfb39c3db52054df298
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-07 05:18:34 +0000

    Home Amor V2

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,9 @@ function Home() {
     return ( 
         <div>
             <center>
-                Oi meu amor!<br>
-                Fiz essa pequena página para<br>
-                Te lembrar que eu<br>
-                <h2 style="color:red;">Te Amo</h2><br>
-                Mesmo você querendo me matar todos os dias!<br></br>
-                <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755"></img>
+                Oi meu amor!
             </center>
         </div>
     )
 }
-
 export default Home;
