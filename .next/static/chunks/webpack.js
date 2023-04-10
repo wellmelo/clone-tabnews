3516b8c29a568d2d7cc3de52525fdabadd352926
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-10 05:40:06 +0000

    Adicionei CSS a Pagina

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19d9e500d0dfc616"; }
+/******/ 		__webpack_require__.h = function() { return "2fdd33ebe16b745a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
