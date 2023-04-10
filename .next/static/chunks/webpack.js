00b752106fc8c80be2b4548cbc3ec786aa5a427f
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-10 05:09:54 +0000

    Adicionei CSS ao Node JS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74fa044e038e636c"; }
+/******/ 		__webpack_require__.h = function() { return "19d9e500d0dfc616"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return (
-        <div>
-            Oi meu amor! Fiz essa pequena página para dizer que eu <h2>Te Amo</h2><br></br>
+        <div class="page-love">
+            <p>Oi meu amor! Fiz essa pequena página para dizer que eu</p>
+            <h1>Te Amo</h1>
             <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755"></img>
         </div>
     )
