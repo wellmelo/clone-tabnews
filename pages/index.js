191dd54066b38140cec2763fd0fab7be4dca7d4f
Wellington Melo <wellingtonmeloweb@gmail.com>
2023-04-20 04:44:03 +0000

    Atualizando Home Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="page-love">
-            <p>Oi meu amor! Fiz essa pequena página para dizer que eu</p>
+            <p>Oi meu amor! Fiz essa pequena página para dizer que...</p>
             <h1>Te Amo!!!</h1>
             <img src="https://static.wikia.nocookie.net/302d5a4f-8ad4-4383-b7a6-11932aee990c/scale-to-width/755"></img>
         </div>
